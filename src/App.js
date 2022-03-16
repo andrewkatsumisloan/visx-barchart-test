e845799eef7717bf878c017624007acbe2da9ec5
Blake Sloan <bsloan@marvelstudios.com>
2022-03-16 16:30:40 -0700

    changed to tsx and other stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import BarGraph from './BarGraph.js';
+import BarGraph from './BarGraph.tsx';
 
 function App() {
   return (
     <div className="App">
-      <BarGraph />
+      <BarGraph width="512" height="512"/>
     </div>
   );
 }
