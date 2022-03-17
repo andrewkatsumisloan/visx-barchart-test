e6fe4692509a263b8a3b03bdc68b3e5a71c239ba
Blake Sloan <bsloan@marvelstudios.com>
2022-03-17 15:17:59 -0700

    fixed graph. whew

--- src/BarGraph.tsx
+++ src/BarGraph.tsx
@@ -29,45 +29,53 @@ export default function BarGraph({
     static_revenue: 1000,
     year: '2020',
   }, {
-    dynamic_revenue: 1000,
+    dynamic_revenue: 100,
     static_revenue: 2000, 
     year: '2021',
   }, {
-    dynamic_revenue: 2000,
-    static_revenue: 4000,
+    dynamic_revenue: 2500,
+    static_revenue: 1500,
     year: '2022',
   }, {
-    dynamic_revenue: 4000,
-    static_revenue: 8000,
+    dynamic_revenue: 2500,
+    static_revenue: 2800,
     year: '2023',
   }, {
-    dynamic_revenue: 8000,
-    static_revenue: 16000,
+    dynamic_revenue: 800,
+    static_revenue: 1600,
     year: '2024',
   }, {
-    dynamic_revenue: 16000, 
-    static_revenue: 32000,
+    dynamic_revenue: 2200, 
+    static_revenue: 1200,
     year: '2025'
   }];
 
-  const data1 = dummyData.slice(0, 5);
+  const data1 = dummyData;
+
   const keys1 = Object.keys(data1[0]).filter((d) => d !== 'year');
   const defaultMargin = { top: 40, right: 0, bottom: 40, left: 0 };
 
   const formatDate = (date: string) => date;
   console.log(keys1)
 
-  const getYear = (d: static_revenue) => d.year;
-  const getDynamicRevenue = (d: static_revenue) => d.dynamic_revenue;
+  const xMax = width - margin.left - margin.right;
+  const yMax = height - margin.top - margin.bottom;
 
-  const dateScale = scaleBand<string>({
+
+  const getYear = (d: static_revenue) => d.year;
+
+  const paramScale = scaleBand<string>({
+    domain: keys1,
+    padding: 0.1,
+  });
+
+  const yearScale = scaleBand<string>({
     domain: data1.map(getYear),
     padding: 0.2,
   });
 
-  const valueScale = scaleBand<string>({
-    domain: data1.map(getDynamicRevenue),
-    padding: 0.2,
+  const valueScale = scaleLinear<number>({
+     domain: [0, Math.max(...data1.map((d) => Math.max(...keys1.map((key) => Number(d[key])))))],
   });
 
   const colorScale = scaleOrdinal<string, string>({
@@ -75,11 +83,9 @@ export default function BarGraph({
     range: [blue, green, purple],
   }); 
 
-  const xMax = width - margin.left - margin.right;
-  const yMax = height - margin.top - margin.bottom;
-
   // update scale output dimensions
-  dateScale.rangeRound([0, xMax]);
+  yearScale.rangeRound([0, xMax]);
+  paramScale.rangeRound([0, yearScale.bandwidth()]);
   valueScale.rangeRound([yMax, 0])
 
   return (
@@ -92,8 +98,8 @@ export default function BarGraph({
           height={yMax}
           x0={getYear}
           x1={getYear}
-          x0Scale={dateScale}
-          x1Scale={dateScale}
+          x0Scale={yearScale}
+          x1Scale={paramScale}
           yScale={valueScale}
           color={colorScale}
         >
@@ -124,7 +130,7 @@ export default function BarGraph({
       <AxisBottom
         top={yMax + margin.top}
         tickFormat={formatDate}
-        scale={dateScale}
+        scale={yearScale}
         stroke={green}
         tickStroke={green}
         hideAxisLine
